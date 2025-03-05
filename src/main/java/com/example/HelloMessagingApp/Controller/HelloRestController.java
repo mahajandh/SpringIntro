4df439a9be8913d97c6b635cf59245f4e776aea9
mahajandh <dhruvmahajan73@gmail.com>
2025-03-05 12:04:19 +0530

    hello with params

--- src/main/java/com/example/HelloMessagingApp/Controller/HelloRestController.java
+++ src/main/java/com/example/HelloMessagingApp/Controller/HelloRestController.java
@@ -1,6 +1,8 @@
 package com.example.HelloMessagingApp.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +10,10 @@ public class HelloRestController {
     @RequestMapping("/")
     public String index(){
         return "Hello from BridgeLabz";
+    }
+
+    @GetMapping("/hello/query")
+    public String sayHello(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz";
     }
 }
--- src/main/java/com/example/HelloMessagingApp/HellomessagingappApplication.java
+++ src/main/java/com/example/HelloMessagingApp/HellomessagingappApplication.java
@@ -4,11 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HelloMessagingAppApplication {
+public class HellomessagingappApplication {
 
 	public static void main(String[] args) {
+
 		System.out.println("hello");
-		SpringApplication.run(HelloMessagingAppApplication.class, args);
+		SpringApplication.run(HellomessagingappApplication.class, args);
 	}
 
 }
